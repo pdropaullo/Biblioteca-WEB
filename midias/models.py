ca98ebf23d23bf3c40950fd11cfb9e6bf88534a6
pdropaullo <pedroabreu11@hotmail.com>
2023-05-18 21:19:16 -0300

    adicionando ferramentas e estilizando

--- midias/models.py
+++ midias/models.py
@@ -1,7 +1,12 @@
+from distutils.command.upload import upload
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
+
 
 
 class Midias(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     release_year = models.IntegerField()
     director = models.TextField()
@@ -14,6 +19,7 @@ class Midias(models.Model):
     # descricao = models.TextField()
     # data_nascimento = models.DateField()
     # ativo = models.BooleanField()
+    #blank=True define como não obrigatorio, opcional
 
     def __str__(self):
         return self.title
--- midias/urls.py
+++ midias/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('busca/', views.search, name='busca')
+    path('busca/', views.search, name='busca'),
+    path('detalhes/<int:id>', views.detalhes, name='detalhes'),
+    path('deletar/<int:id>', views.deletar, name='deletar'),
+    path('adicionar/', views.adicionar, name='adicionar'),
+    path('editar/<int:id>', views.editar, name='editar')
 ]
